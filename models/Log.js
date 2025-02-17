88f9234a57cb0be9b75868d1a11dbffca2336b58
𝐌𝐫 𝐅𝐫𝐚𝐧𝐤 𝐎𝐅𝐂 <167776975+mrfrankzw@users.noreply.github.com>
2025-02-17 17:51:09 +0200

    Delete models/Log.js

--- models/Log.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const LogSchema = new mongoose.Schema({
-  sessionId: String,
-  prefix: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Log', LogSchema);
